7d0b7e3d533c73030855759260431a673c44c474
Alexei Sholik <alcosholik@gmail.com>
2014-06-11 04:18:46 +0300

    Update doc generation settings

--- mix.exs
+++ mix.exs
@@ -4,12 +4,28 @@ defmodule Porcelain.Mixfile do
   def project do
     [app: :porcelain,
      version: "1.0.0-alpha",
-     elixir: "~> 0.13.3 or ~> 0.14.0"]
+     elixir: "~> 0.13.3 or ~> 0.14.0",
+     docs: docs]
   end
 
   # Configuration for the OTP application
   def application do
     []
+  end
+
+  def docs do
+    [
+      formatter: "sphinx",
+      #highlighter: :"highligh.js" | :"pygments",
+      readme: true,
+
+      formatter_opts: [
+        gen_overview: false,
+        pygments_style: "emacs",
+        html_theme: "pyramid",
+        html_type: "singlehtml"
+      ]
+    ]
   end
 
   # no deps
