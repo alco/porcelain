cc469c7b221af3004db253df9b02fb361b2e3bbb
Alexei Sholik <alcosholik@gmail.com>
2014-06-15 18:36:28 +0300

    Use a queue in stream server

--- lib/porcelain/drivers/stream_server.ex
+++ lib/porcelain/drivers/stream_server.ex
@@ -4,12 +4,12 @@ defmodule Porcelain.Driver.Basic.StreamServer do
   # Internal module used to make output streams work
 
   require Record
-  Record.defrecordp :state, [:done, :data, :client]
+  Record.defrecordp :state, [:done, :chunks, :client]
 
   use GenServer
 
   def start() do
-    GenServer.start(__MODULE__, state(data: []))
+    GenServer.start(__MODULE__, state(chunks: :queue.new))
   end
 
   def get_data(pid) do
@@ -18,7 +18,7 @@ defmodule Porcelain.Driver.Basic.StreamServer do
   end
 
   def put_data(pid, data) do
-    log "Stream server put data"
+    log "Stream server put data #{inspect data}"
     GenServer.cast(pid, {:data, data})
   end
 
@@ -29,31 +29,37 @@ defmodule Porcelain.Driver.Basic.StreamServer do
 
   ###
 
-  def handle_call(:get_data, _from, state(done: true, data: data)) do
-    reply = if data == [], do: nil, else: data
-    log "Stream server did stop with reply"
-    {:stop, :shutdown, reply, nil}
+  def handle_call(:get_data, _from, state(done: true, chunks: q)) do
+    if :queue.is_empty(q) do
+      log "Stream server did stop"
+      {:stop, :shutdown, nil, nil}
+    else
+      log "Stream server reply"
+      {:reply, :queue.head(q), state(chunks: :queue.tail(q))}
+    end
   end
 
-  def handle_call(:get_data, from, state(data: [])=state) do
-    log "get_data: []"
-    {:noreply, state(state, client: from)}
+  def handle_call(:get_data, from, state(chunks: q)=state) do
+    if :queue.is_empty(q) do
+      log "get_data: []"
+      {:noreply, state(state, client: from)}
+    else
+      log "get_data: <data>"
+      {:reply, :queue.head(q), state(state, chunks: :queue.tail(q))}
+    end
   end
 
-  def handle_call(:get_data, _from, state(data: data)=state) do
-    log "get_data: <data>"
-    {:reply, data, state(state, data: [])}
-  end
 
-
-  def handle_cast({:data, new_data}, state(data: data, client: nil)=state) do
+  def handle_cast({:data, data}, state(chunks: q, client: nil)=state) do
     log "Stream server got data"
-    {:noreply, state(state, data: [data, new_data])}
+    {:noreply, state(state, chunks: :queue.in(data, q))}
   end
 
-  def handle_cast({:data, new_data}, state(data: [], client: client)=state) do
+  def handle_cast({:data, data}, state(chunks: q, client: client)=state) do
+    true = :queue.is_empty(q)
+
     log "Stream server got data. Sending to client"
-    GenServer.reply(client, new_data)
+    GenServer.reply(client, data)
     {:noreply, state(state, client: nil)}
   end
 
@@ -62,16 +68,15 @@ defmodule Porcelain.Driver.Basic.StreamServer do
     {:noreply, state(state, done: true)}
   end
 
-  def handle_cast(:done, state(data: [], client: client)) do
-    log "Stream server did stop"
-    GenServer.reply(client, nil)
-    {:stop, :shutdown, nil}
-  end
-
-  def handle_cast(:done, state(data: data, client: client)) do
-    log "Stream server did stop"
-    GenServer.reply(client, data)
-    {:stop, :shutdown, nil}
+  def handle_cast(:done, state(chunks: q, client: client)=state) do
+    if :queue.is_empty(q) do
+      log "Stream server did stop"
+      GenServer.reply(client, nil)
+      {:stop, :shutdown, nil}
+    else
+      GenServer.reply(client, :queue.head(q))
+      {:noreply, state(state, chunks: :queue.tail(q))}
+    end
   end
 
   defp log(_), do: nil
